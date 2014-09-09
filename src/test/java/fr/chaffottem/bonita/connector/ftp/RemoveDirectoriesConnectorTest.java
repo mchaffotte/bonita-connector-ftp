1e62a07fa273c9f61f3a2f89743f518034a50c4e
Matthieu Chaffotte <matthieu.chaffotte@bonitasoft.com>
2014-09-09 17:31:52 +0200

    Add a connector to remove directories

---
+++ src/test/java/fr/chaffottem/bonita/connector/ftp/RemoveDirectoriesConnectorTest.java
@@ -0,0 +1,120 @@
+/**
+ * Copyright (C) 2014 Matthieu Chaffotte
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>
+ */
+package fr.chaffottem.bonita.connector.ftp;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+
+/**
+ * @author Matthieu Chaffotte
+ */
+public class RemoveDirectoriesConnectorTest extends FTPClientConnectorTest {
+
+    @Override
+    public List<String> getServerDirecotries() {
+        return Arrays.asList("c:\\share", "c:\\share\\images", "c:\\share\\docs");
+    }
+
+    @Override
+    public Map<String, String> getServerFiles() {
+        final Map<String, String> files = new HashMap<String, String>();
+        files.put("c:\\share\\run.exe", "");
+        files.put("c:\\share\\docs\\file1.txt", "qsfojsdfmljsgih");
+        return files;
+    }
+
+    @Override
+    public String getUserDirectory() {
+        return "c:\\share";
+    }
+
+    @Override
+    public FTPClientConnector getFTPClientConnector() {
+        return new RemoveDirectoriesConnector();
+    }
+
+    private Boolean getStatusOfEntry(final Map<String, Object> result, final String pathname) {
+        final Map<String, Boolean> status = getStatus(result);
+        return status.get(pathname);
+    }
+
+    @SuppressWarnings("unchecked")
+    private Map<String, Boolean> getStatus(final Map<String, Object> result) {
+        return (Map<String, Boolean>) result.get(RemoveDirectoriesConnector.STATUS);
+    }
+
+    @Test
+    public void removeAnEmptyDirectory() throws Exception {
+        final Map<String, Object> paramaters = new HashMap<String, Object>();
+        paramaters.put(FTPClientConnector.HOSTNAME, HOSTNAME);
+        paramaters.put(FTPClientConnector.PORT, getListeningPort());
+        paramaters.put(FTPClientConnector.USER_NAME, USER_NAME);
+        paramaters.put(FTPClientConnector.PASSWORD, PASSWORD);
+        paramaters.put(RemoveDirectoriesConnector.PATHNAMES, Arrays.asList("c:\\share\\images"));
+
+        final Map<String, Object> result = execute(paramaters);
+
+        assertThat(getDirectory("c:\\share\\images")).isNull();
+        assertThat(getStatusOfEntry(result, "c:\\share\\images")).isTrue();
+    }
+
+    @Test
+    public void removeAFullDirectoryDoesAnything() throws Exception {
+        final Map<String, Object> paramaters = new HashMap<String, Object>();
+        paramaters.put(FTPClientConnector.HOSTNAME, HOSTNAME);
+        paramaters.put(FTPClientConnector.PORT, getListeningPort());
+        paramaters.put(FTPClientConnector.USER_NAME, USER_NAME);
+        paramaters.put(FTPClientConnector.PASSWORD, PASSWORD);
+        paramaters.put(RemoveDirectoriesConnector.PATHNAMES, Arrays.asList("c:\\share\\docs"));
+
+        final Map<String, Object> result = execute(paramaters);
+
+        assertThat(getDirectory("c:\\share\\docs")).isNotNull();
+        assertThat(getFile("c:\\share\\docs\\file1.txt")).isNotNull();
+        assertThat(getStatusOfEntry(result, "c:\\share\\docs")).isFalse();
+    }
+
+    @Test
+    public void removeAnUnexistingDirectory() throws Exception {
+        final Map<String, Object> paramaters = new HashMap<String, Object>();
+        paramaters.put(FTPClientConnector.HOSTNAME, HOSTNAME);
+        paramaters.put(FTPClientConnector.PORT, getListeningPort());
+        paramaters.put(FTPClientConnector.USER_NAME, USER_NAME);
+        paramaters.put(FTPClientConnector.PASSWORD, PASSWORD);
+        paramaters.put(RemoveDirectoriesConnector.PATHNAMES, Arrays.asList("c:\\share\\video"));
+
+        final Map<String, Object> result = execute(paramaters);
+
+        assertThat(getStatusOfEntry(result, "c:\\share\\video")).isFalse();
+    }
+
+    @Test
+    public void removeNothing() throws Exception {
+        final Map<String, Object> paramaters = new HashMap<String, Object>();
+        paramaters.put(FTPClientConnector.HOSTNAME, HOSTNAME);
+        paramaters.put(FTPClientConnector.PORT, getListeningPort());
+        paramaters.put(FTPClientConnector.USER_NAME, USER_NAME);
+        paramaters.put(FTPClientConnector.PASSWORD, PASSWORD);
+
+        final Map<String, Object> result = execute(paramaters);
+        assertThat(getStatus(result)).isEmpty();
+    }
+
+}
