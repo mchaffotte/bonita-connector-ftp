cd39bb34ed02ea31255f87fc4f74a5554812d655
Matthieu Chaffotte <matthieu.chaffotte@bonitasoft.com>
2014-09-16 17:12:03 +0200

    Add main test suite

---
+++ src/test/java/fr/chaffottem/bonita/connector/ftp/FTPConnectorTests.java
@@ -0,0 +1,17 @@
+package fr.chaffottem.bonita.connector.ftp;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+    ListFilesConnectorTest.class,
+    DeleteFilesConnectorTest.class,
+    RemoveDirectoriesConnectorTest.class,
+    MakeDirectoriesConnectorTest.class,
+    UploadFilesConnectorTest.class
+})
+public class FTPConnectorTests {
+
+}
